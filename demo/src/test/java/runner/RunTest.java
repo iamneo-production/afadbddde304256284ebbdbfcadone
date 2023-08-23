36ad5d318a810a2e259b405b48a7f434101ad795
Iamneo <techadmin@iamneo.ai>
2023-08-23 11:17:18 +0000

    Backup Commit

--- demo/src/test/java/runner/RunTest.java
+++ demo/src/test/java/runner/RunTest.java
@@ -10,6 +10,14 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features",
-				glue = "glue")
+				glue = "glue",
+				plugin = {
+					"pretty", 
+					"json:Cucumber-Reports/CucumberReport.json",
+					"rerun:Cucumber-Reports/rerun.txt",
+					"html:Cucumber-Reports/CucumberReport.html", 
+					"junit:junit_xml/CucumberReport.xml",
+					"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" 
+					})
 public class RunTest {
 }
