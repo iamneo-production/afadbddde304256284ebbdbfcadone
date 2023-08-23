1b1f005f55a28f570dbc06321cb9f303192908ba
Iamneo <techadmin@iamneo.ai>
2023-08-23 11:43:28 +0000

    Backup Commit

--- demo/src/test/java/runner/RunTest.java
+++ demo/src/test/java/runner/RunTest.java
@@ -1,22 +1,5 @@
 package runner;
 
-import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-// import io.cucumber.junit.Cucumber;
-// import io.cucumber.junit.CucumberOptions;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features",
-				glue = "glue",
-				plugin = {
-					"pretty", 
-		
-					"html:Cucumber-Reports/CucumberReport.html", 
-					
-					
-					})
-public class RunTest {
+public enum RunTest {
+    
 }
